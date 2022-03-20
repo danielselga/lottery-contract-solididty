f5479c3270098c8d08f541263ed48c007fe0fdbd
Daniel Selga <danielselga@gmail.com>
2022-03-20 20:33:45 -0300

    chore: contract changes

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address[] public players; // When we mark an array as public we only can retrive one value per time not the entire array.
 
     constructor() {
         manager = msg.sender;
@@ -12,5 +12,5 @@ contract Lottery {
 
     function enter() public payable {
         players.push(msg.sender);
-    } 
-}
+    }
+}   
